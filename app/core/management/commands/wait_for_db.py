436c6ee37f117e3adb6b460959ff2f5fbfba1222
anwar448 <80207856+anwar448@users.noreply.github.com>
2024-12-06 20:10:24 +0100

    blank line
    blank line

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,9 +2,7 @@
 Django command to wait for the database to be available
 """
 import time
-
 from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -25,5 +23,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
-        
